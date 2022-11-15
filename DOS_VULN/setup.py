1a1bfb6814bba1b11def12eb4be6cab36d840930
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2022-11-16 03:12:56 +0700

    Update setup.py

--- DOS_VULN/setup.py
+++ DOS_VULN/setup.py
@@ -9,5 +9,11 @@ pre_import = open("ETC\\prefix.txt",'w')
 pre_import.write(str(prefix_get))
 pre_import.close()
 
+print("True or False")
+auto_update = input("auto:")
+import_file_write = open("ETC\\update\\check_update.txt","w")
+import_file_write.write(str(auto_update))
+import_file_write.close()
+
 print("removeing setup.py")
 os.remove(__file__)
