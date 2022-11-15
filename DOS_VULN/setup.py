560cfd2216b654f22b9bdbeadd8dd110c3c8ca8c
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2022-11-15 23:02:49 +0700

    Update setup.py

--- DOS_VULN/setup.py
+++ DOS_VULN/setup.py
@@ -4,9 +4,8 @@ SETUP CNC OR CONTROL PANEL . . .
 1] PREFIX COMMAND SETUP
 2] UPDATE CHECK (COMING SOON)
 """)
-print("example prefix command: . / - ! ? $ #")
 prefix_get = input("prefix:")
-pre_import = open("ETC\\prefix.txt",'w')
+pre_import = open("ETC\\\prefix.txt",'w')
 pre_import.write(str(prefix_get))
 pre_import.close()
 
