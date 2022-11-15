537b65e9148de9aca4332e2e27a4b603d2843e26
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2022-11-15 23:17:12 +0700

    Update DOS_VULN.py

--- DOS_VULN/DOS_VULN.py
+++ DOS_VULN/DOS_VULN.py
@@ -4,27 +4,47 @@ from GUI import *
 from ETC.prefix_find import *
 from command import *
 from ETC.update.check_update import *
-
+def download_file2(url, local_file_path,type):
+    response = requests.get(url=url)
+    if response.status_code == 200:
+        if "w" in type:
+            if " " == response.text or "" == response.text or "  " == response.text:
+                with open(local_file_path, 'w') as local_file:
+                    local_file.write(response.text)
+            else:
+                print("error write pls wait. . . .")
+        else:
+            if " " == response.text or "" == response.text or "  " == response.text:
+                with open(local_file_path, 'wb') as local_file:
+                    local_file.write(response.text)
+            else:
+                print("error write pls wait. . . .")
 download_file("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/ver_of_dos.txt","ETC\\update\\ver_github_of_dos.txt")
-
 read_as_update = open("ETC\\update\\ver_github_of_dos.txt","r")
 data_update = read_as_update.read()
 read_as_update.close()
-
 up_file = get_update("get now . . .")
 update_patch = find_update(data_update)
+def loader_update():
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/setup.py","setup.py","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/ETC/update/check_update.py","ETC\\update\\check_update.py","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/ver_of_dos.txt","ETC\\update\\ver_of_dos.txt","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/command.py","command.py","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/DOS_VULN.py","DOS_VULN.py","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/ETC/prefix_find.py","ETC\\prefix_find.py","w")
+    download_file2("https://raw.githubusercontent.com/Hex1629/update_vuln_dos_page/main/DOS_VULN/GUI.py","GUI.py","w")
+    print("DONE UPDATE NEW!! AND OPEN AGAIN!!")
+    time.sleep(2)
+    exit()
 if True == update_patch:
     print("FOUND NEW UPDATE . . .")
     print(f"VERSION OF DOS YOU:{up_file}")
     print(F"VERSION OF DOS NOW:{data_update}")
+    loader_update()
 else:
     print("NOT UPDATE . . .")
-
-time.sleep(2)
-
 prefix = find_prefix("GET PREFIX NOW . . .")
 banner()
-
 while True:
     command = input(f"{Fore.LIGHTGREEN_EX}[{Fore.RESET}{Fore.WHITE}>_{Fore.RESET}{Fore.LIGHTGREEN_EX}]{Fore.RESET} ")
     command_attack(command)
