fdf377a8ba660b8e068d69933aabc78540f19c29
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2022-11-15 23:42:32 +0700

    Update prefix_find.py

--- DOS_VULN/ETC/prefix_find.py
+++ DOS_VULN/ETC/prefix_find.py
@@ -1,5 +1,5 @@
 def find_prefix(get):
-    pre_import = open("ETC\\\\prefix.txt",'r')
+    pre_import = open("ETC\\prefix.txt",'r')
     pre_command = pre_import.read()
     pre_import.close()
     get = ""
